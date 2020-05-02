a4baa13b030a18aaecb4707d1b13e2d294254925
mikecrbenton <mikecrbenton@hotmail.com>
2020-05-02 13:19:25 -0500

    finished the easier javascript functions

--- index.js
+++ index.js
@@ -203,48 +203,72 @@ const artists = [
 
 // 🖌🖼 M V P 🖼🖌 //
 
-/* Task 1: Practice accessing data above by console.log-ing following items:
+//======================================================================================
+/* Task 1: Practice accessing data above by console.log-ing following items:*/
+//======================================================================================
 
-(1) Name of the first artist in the array
-(2) Bio of the third artist in the array */
+//(1) Name of the first artist in the array
+console.log(artists[0].name);
+
+//(2) Bio of the third artist in the array 
+console.log(artists[2].name);
 
 
 
-/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+//======================================================================================
+/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently 
+Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+//======================================================================================
+
+artists[8].name = "Vincent Van Gough";
+
+//RUN CODE
+//console.log(artists[8]);
 
 
 
+//======================================================================================
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
  * getArtistByIndex returns a string in the format `The artist at index {id} is {name}.`
  * 
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
- * it will return `The artist at index 0 is Amedeo Modigliani`.
-*/
-function getArtistByIndex(id, name) {
-    /* code here */
-  }
+ * it will return `The artist at index 0 is Amedeo Modigliani`. */
+//======================================================================================
+
+function getArtistByIndex( myArray, num ) {
+    
+  console.log( `The artist at index ${num} is ${artists[num].name}` );
+  return `The artist at index ${num} is ${artists[num].name}`;
+}
+
+//CALL THE FUNCTION
+getArtistByIndex( artists, 2);
   
-  /**
+  
 
-
+//======================================================================================
 /* Task 4: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
  * removeArtist removes an artist from the dat array at the index.
  * 
  * For example, if removeArtist is invoked with the data and the number 0,
- * it will remove Amedeo Modigliani from our dataset.
-*/
+ * it will remove Amedeo Modigliani from our dataset.  */
+//======================================================================================
+
+
 function removeArtist(/*code here*/) {
     /* code here */
   }
   
   /**
 
-
-/* Task 5: Create a function called lotsOfArt() that takes artists as an argument and returns an array with names of artists who painted more than 100 paintings */
+//======================================================================================
+/* Task 5: Create a function called lotsOfArt() that takes artists as an argument and 
+returns an array with names of artists who painted more than 100 paintings */
+//======================================================================================
 
 function lotsOfArt(/* Code here */){
 
@@ -252,8 +276,10 @@ function lotsOfArt(/* Code here */){
 
   }
 
-
-/* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, Add a 21st artist to the array (you) with custom information! 👩‍🎨👨‍🎨
+//======================================================================================
+/* Task 6: Create a function called `addArtist` that can accept an array of information 
+and add it to the artists array. Then, Add a 21st artist to the array (you) with custom information! 👩‍🎨👨‍🎨
+//======================================================================================
 
 id: 21
 name: Your Name Here, 
@@ -273,8 +299,10 @@ function addArtist(/* Code here */){
 
 
 // 🎨🎨 STRETCH 🎨🎨//
-
-/* STRETCH 1: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1800-1900) */
+//======================================================================================
+/* STRETCH 1: Create a function called get20s() that takes data as an argument and returns 
+an array with names of artists who were born the 20th century (1800-1900) */
+//======================================================================================
 
 function get20s(/* Code here */){
 
